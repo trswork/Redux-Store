87dfbcec1b8cc64403da18d471ae5d2e5f5de945
trswork <87744214+trswork@users.noreply.github.com>
2021-12-27 22:30:56 -0500

    update

--- client/src/utils/reducers.js
+++ client/src/utils/reducers.js
@@ -1,3 +1,4 @@
+import { useReducer } from 'react';
 import {
     UPDATE_PRODUCTS,
     UPDATE_CATEGORIES,
@@ -10,11 +11,26 @@ import {
       case UPDATE_PRODUCTS:
         return {
           ...state,
-          products: [...action.products],
+          products: [...action.products]
+        };
+      // if action type value is the value of `UPDATE_CATEGORIES`, return a new state object with an updated categories array
+      case UPDATE_CATEGORIES:
+        return {
+          ...state,
+          categories: [...action.categories]
+        };
+
+      case UPDATE_CURRENT_CATEGORY:
+        return {
+        ...state,
+        currentCategory: action.currentCategory
         };
   
-      // if it's none of these actions, do not update state at all and keep things the same!
       default:
         return state;
     }
   };
+
+  export function useProductReducer(initialState) {
+    return useReducer(reducer, initialState);
+  }
