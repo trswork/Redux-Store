8128aa354ae7949f083e20a38cc0fe7930f7ccb0
trswork <87744214+trswork@users.noreply.github.com>
2021-12-27 22:37:40 -0500

    update

--- client/src/App.js
+++ client/src/App.js
@@ -9,6 +9,7 @@ import NoMatch from "./pages/NoMatch";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import Nav from "./components/Nav";
+import { StoreProvider } from "./utils/GlobalState";
 import OrderHistory from "./pages/OrderHistory";
 import Success from './pages/Success'
 
@@ -17,22 +18,20 @@ function App() {
       <ApolloProvider client={client}>
         <Router>
           <div>
-            <Provider store={store}>
-            <Nav />
-            <Switch>
-              <Route exact path="/" component={Home} />
-              <Route exact path="/login" component={Login} />
-              <Route exact path="/signup" component={Signup} />
-              <Route exact path="/orderHistory" component={OrderHistory} />
-              <Route exact path="/products/:id" component={Detail} />
-              <Route exact path="/success" component={Success} />
-              <Route component={NoMatch} />
-            </Switch>
-            </Provider>
+            <StoreProvider>
+              <Nav />
+              <Switch>
+                <Route exact path="/" component={Home} />
+                <Route exact path="/login" component={Login} />
+                <Route exact path="/signup" component={Signup} />
+                <Route exact path="/orderHistory" component={OrderHistory} />
+                <Route exact path="/products/:id" component={Detail} />
+                <Route component={NoMatch} />
+              </Switch>
+            </StoreProvider>
           </div>
         </Router>
       </ApolloProvider>
-  
     );
   }
   
